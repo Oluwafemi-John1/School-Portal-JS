8d87c04a60279b7f6f12ffa5589fb65a73f8391b
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2024-01-17 11:43:52 +0100

    Refactor submit function to declare variables inside the function scope

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var firstname = document.getElementById('firstname').value
-var lastname = document.getElementById('lastname').value
-var mailphone = document.getElementById('mailphone').value
-var password = document.getElementById('password').value
 
 function submit() {
+    var firstname = document.getElementById('firstname').value
+    var lastname = document.getElementById('lastname').value
+    var mailphone = document.getElementById('mailphone').value
+    var password = document.getElementById('password').value
+    
     console.log(firstname, lastname, mailphone, password);
 }
