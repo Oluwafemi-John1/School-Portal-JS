f6401737ab1bf867e12038bb3cf5746852c570fa
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2024-01-17 11:49:04 +0100

    Add form validation and clear input fields after submission

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ function submit() {
     var lastname = document.getElementById('lastname').value
     var mailphone = document.getElementById('mailphone').value
     var password = document.getElementById('password').value
+
+    if (firstname === '' || lastname === '' || mailphone === '' || password === '') {
+        alert("haba, haba, you no dey see")
+    } else {
+        var studentObj = {firstname,lastname,mailphone,password}
+        console.log(studentObj);
     
-    console.log(firstname, lastname, mailphone, password);
+        firstname = ''
+        lastname = ''
+        mailphone = ''
+        password = ''
+    }
 }
