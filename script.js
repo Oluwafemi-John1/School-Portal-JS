291cb9c55c5581235e1134f7e3b31372b31aea8e
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2024-01-17 11:42:04 +0100

    Add form submission functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+var firstname = document.getElementById('firstname').value
+var lastname = document.getElementById('lastname').value
+var mailphone = document.getElementById('mailphone').value
+var password = document.getElementById('password').value
+
+function submit() {
+    console.log(firstname, lastname, mailphone, password);
+}
