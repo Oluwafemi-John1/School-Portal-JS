55b0e7cb791ecdf26c9fa5d8c015ccc8f9127864
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2024-01-18 10:17:49 +0100

    Add settings for live server and display empty error message

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function submit() {
     var password = document.getElementById('password').value
 
     if (firstname === '' || lastname === '' || mailphone === '' || password === '') {
-        alert("haba, haba, you no dey see")
+        emptyErrorMessage.style.display = 'block'
+        setTimeout(() => {
+            emptyErrorMessage.style.display = 'none'
+        }, 5000);
     } else {
         var studentObj = {firstname,lastname,mailphone,password}
         allStudents.push(studentObj)
